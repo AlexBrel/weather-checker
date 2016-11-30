2af19f5073ac8d6710fdb9e77d8b9631fb5dfea3
AlexBrel <aliaksandr.brel@gmail.com>
2016-12-01 00:37:52 +0200

    Added angular 2

---
+++ app/js/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `<div>
+        I'm your first component
+        
+    </div>`
+})
+export class AppComponent {
+}
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,17 +1,25 @@
-const webpack = require('webpack');
+const webpack = require('webpack'),
+	ManifestRevisionPlugin = require('manifest-revision-webpack-plugin'),
+    TextPlugin   = require('extract-text-webpack-plugin'),
+	HtmlWebpackPlugin   = require('html-webpack-plugin'),
+    rootAssetPath = './app/assets';
 
 module.exports = {
-	entry: './app/js/app.ts',
+	entry: {
+        main: './app/js/main.ts',
+		vendor: './app/vendor.ts'
+	},
 	output: {
 		path: "./app/dist",
-		filename: "bundle.js"
+		filename: "[name].bundle.js"
 	},
 	resolve: {
 		extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
 	},
 	module: {
 		loaders: [
-			{test: /\.tsx?$/, loader: 'ts-loader'}
+			{test: /\.tsx?$/, loader: 'ts-loader'},
+            { test: /\.css?$/, loaders: ['file?context=' + rootAssetPath + '&name=assets/css/[name].[ext]'] }
 		]
 	},
 	plugins: [
@@ -19,6 +27,14 @@ module.exports = {
 			names: ['common'],
 			filename: '[name].bundle.js',
 			minChunks: 2
-		})
+		}),
+        new ManifestRevisionPlugin(__dirname + '/manifest.json',{
+            rootAssetPath: rootAssetPath,
+            ignorePaths: []
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './app/index.html'
+        })
 	]
 };
